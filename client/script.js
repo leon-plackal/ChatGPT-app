93838b75c0eacbe55e95a7dbeb484c9fdaa366a7
Leon Plackal <110382152+leon-plackal@users.noreply.github.com>
2023-05-15 19:38:49 +1000

    Added header and colours

--- client/script.js
+++ client/script.js
@@ -1,24 +1,104 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat-container')
+
+let loadInterval;
+
+// ... loader
+function loader(element) {
+  element.textContent = '';
+  loadInterval = setInterval(() => {
+    element.textContent += '.'
+    if(element.textContent === '....'){
+      element.textContent = ''
+    }
+  }, 300)
+}
+
+// loading by character
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index)
+      index++
+    } else{
+      clearInterval(interval)
+    }
+  }, 20)
+}
+
+// message IDs
+function generateUniqueID(){
+  const timeStamp = Date.now()
+  const randomNumber = Math.random()
+  const hexaString = randomNumber.toString(16)
+
+  return `id-${timeStamp}-${hexaString}`
+}
+
+// chat colours and icons
+function chatStripe (isAi, value, uniqueId){
+  return (
+    `
+    <div class='wrapper ${isAi && 'ai'}'>
+      <div class='chat'>
+        <div class='profile'>
+        <img src='${isAi ? bot : user}' alt='${isAi ? 'bot' : 'user'}'/>
+        </div>
+        <div class='message' id='${uniqueId}'>
+          ${value}
+        </div>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    `
+  )
+}
 
-setupCounter(document.querySelector('#counter'))
+const handleSubmit = async (e)=>{
+  e.preventDefault();
+
+  const data = new FormData(form)
+  //user stripe
+  chatContainer.innerHTML+=chatStripe(false, data.get('prompt'))
+  form.reset() //clear chat afterwards
+
+  //AI stripe
+  const uniqueId = generateUniqueID()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  chatContainer.scrollTop = chatContainer.scrollHeight
+
+  const messageDiv = document.getElementById(uniqueId)
+  loader(messageDiv)
+  // fetch data from server
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ''
+
+  if(response.ok){
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+    
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+    messageDiv.innerHTML= 'Something went wrong'
+    alert(err)
+  }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e)=>{
+  if(e.keyCode === 13){
+    handleSubmit(e)
+  }
+})
